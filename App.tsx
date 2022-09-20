7c15e7da32fd75403b82a3f53e31cfd96913404f
Eduardo Angelo Simon <eduardosimon.trabalho@gmail.com>
2022-09-20 19:31:58 -0300

    Implementação da navegação

---
+++ App.tsx
@@ -0,0 +1,36 @@
+import React, {type PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  Text,
+  View,
+} from 'react-native';
+
+import Home from './src/screens/Home';
+import Configuration from './src/screens/Configuration';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Home" component={Home} />
+      <Drawer.Screen name="Configuration" component={Configuration} />
+    </Drawer.Navigator>
+  );
+}
+
+const App = () => {
+
+  return (
+    <>
+      <NavigationContainer>
+        <MyDrawer/>
+      </NavigationContainer>
+    </>
+  );
+};
+
+export default App;
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const Home: React.FC = () => {
+  return(
+    <>
+        <SafeAreaView>
+            <Text>Teste Home</Text>
+        </SafeAreaView>
+    </>
+  );
+}
+
+export default Home;
